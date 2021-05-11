db95699f9daa84341c17905a0938f263ce1080c0
Yalda Karimi <yalda.karimi88@gmail.com>
2021-05-11 20:16:02 +0430

    strapi settings

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f72d9815d65e65ca537b509e8ad18751'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a320139-2e99-4319-b4e8-fb6194b54b97'
+};
